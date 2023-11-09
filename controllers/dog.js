07218a7e4387770f98da1e8eb2bbd4a9afc66466
nikhithasree <78521584+nikhithasree@users.noreply.github.com>
2023-11-09 14:52:40 -0600

    Added changes

--- controllers/dog.js
+++ controllers/dog.js
@@ -8,25 +8,10 @@ exports.dog_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: hats detail: ' + req.params.id);
 };
 // Handle hats create on POST.
-exports.dog_create_post = async function(req, res) {
-    console.log(req.body)
-let document = new dog();
-// We are looking for a body, since POST does not have query parameters.
-// Even though bodies can be in many different formats, we will be picky
-// and require that it be a json object
-// {"hats_type":"goat", "cost":12, "size":"large"}
-document.breed = req.body.breed;
-document.color = req.body.color;
-document.cost = req.body.cost;
-try{
-    let result = await document.save();
-    res.send(result);
-}
-catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-}
+exports.costume_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Costume create POST');
 };
+
 // Handle dogs delete form on DELETE.
 exports.dog_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: dogs delete DELETE ' + req.params.id);
