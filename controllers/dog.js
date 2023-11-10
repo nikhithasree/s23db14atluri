ee8c5c445fb815a2e1f3ea68eff01f38d3e7c974
nikhithasree <78521584+nikhithasree@users.noreply.github.com>
2023-11-10 09:09:31 -0600

    Modified changes

--- controllers/dog.js
+++ controllers/dog.js
@@ -7,6 +7,18 @@ res.send('NOT IMPLEMENTED: dog list');
 exports.dog_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: dog detail: ' + req.params.id);
 };
+// for a specific dog.
+exports.dog_detail = async function(req, res) {
+console.log("detail" + req.params.id)
+try {
+result = await dog.findById( req.params.id)
+res.send(result)
+} catch (error) {
+res.status(500)
+res.send(`{"error": document for id ${req.params.id} not found`);
+}
+};
+
 // Handle dog create on POST.
 exports.dog_create_post = async function(req, res) {
     console.log(req.body)
--- routes/dog.js
+++ routes/dog.js
@@ -4,6 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('dog', { title: 'Search Results dog' });
+  //GET request for one costume.
+  router.get('/dog/:id', dog_controller.dog_detail)
 });
 
 module.exports = router;
