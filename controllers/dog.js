c7ac53aa3383d05a2b2b0c92fcff5345baaf7162
nikhithasree <78521584+nikhithasree@users.noreply.github.com>
2023-11-15 22:07:21 -0600

    Added changes

--- controllers/dog.js
+++ controllers/dog.js
@@ -1,87 +1,148 @@
 var dog = require('../models/dog');
 // List of all dog
-exports.dog_list = function(req, res) {
-res.send('NOT IMPLEMENTED: dog list');
+exports.dog_list = function (req, res) {
+    res.send('NOT IMPLEMENTED: dog list');
 };
 // for a specific dog.
-exports.dog_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: dog detail: ' + req.params.id);
-};
-// for a specific dog.
-exports.dog_detail = async function(req, res) {
-console.log("detail" + req.params.id)
-try {
-result = await dog.findById( req.params.id)
-res.send(result)
-} catch (error) {
-res.status(500)
-res.send(`{"error": document for id ${req.params.id} not found`);
-}
+exports.dog_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await dog.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
 
 // Handle dog create on POST.
-exports.dog_create_post = async function(req, res) {
+exports.dog_create_post = async function (req, res) {
     console.log(req.body)
     let document = new dog();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"dog_type":"goat", "cost":12, "size":"large"}
+    // {"hats_type":"goat", "cost":12, "size":"large"}
     document.breed = req.body.breed;
     document.color = req.body.color;
     document.cost = req.body.cost;
-    try{
+    try {
         let result = await document.save();
         res.send(result);
     }
-    catch(err){
+    catch (err) {
         res.status(500);
         res.send(`{"error": ${err}}`);
     }
 };
-
 // Handle dog delete form on DELETE.
-exports.dog_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: dog delete DELETE ' + req.params.id);
-};
-exports.dog_update_put = async function(req, res) {
-    console.log(`update on id ${req.params.id} with body
-    ${JSON.stringify(req.body)}`)
+exports.dog_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
     try {
-    let toUpdate = await dog.findById( req.params.id)
-    // Do updates of properties
-    if(req.body.breed)
-    toUpdate.breed = req.body.breed;
-    if(req.body.color) toUpdate.color = req.body.color;
-    if(req.body.cost) toUpdate.cost = req.body.cost;
-    let result = await toUpdate.save();
-    console.log("Sucess " + result)
-    res.send(result)
+        result = await dog.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
     } catch (err) {
-    res.status(500)
-    res.send(`{"error": ${err}: Update for id ${req.params.id}
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
+};
+// Handle dog update form on PUT.
+exports.dog_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await dog.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.cost)
+            toUpdate.cost = req.body.cost;
+        if (req.body.breed) toUpdate.breed = req.body.breed;
+        if (req.body.color) toUpdate.color = req.body.color;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
     failed`);
     }
-    };
+};
 
-exports.dog_list = async function(req, res) {
-    try{
-    thedog = await dog.find();
-    res.send(thedog);
-    }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-    }
-    };
 
-    exports.dog_view_all_Page = async function(req, res) {
-        try{
+
+exports.dog_list = async function (req, res) {
+    try {
         thedog = await dog.find();
-        res.render('dog', { title: 'dog Search Results', DBresults: thedog });
-        }
-        catch(err){
+        res.send(thedog);
+    }
+    catch (err) {
         res.status(500);
         res.send(`{"error": ${err}}`);
-        }
-        };
+    }
+};
+
+exports.dog_view_all_Page = async function (req, res) {
+    try {
+        thedog = await dog.find();
+        res.render('dog', { title: 'dog Search Results', DBresults: thedog });
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
+// Handle a show one view with id specified by query
+exports.dog_view_one_Page = async function (req, res) {
+    console.log("single view for id " + req.query.id)
+    try {
+        result = await dog.findById(req.query.id)
+        res.render('dogdetail',
+            { title: 'dog Detail', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle building the view for creating a dog.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.dog_create_Page = function (req, res) {
+    console.log("create view")
+    try {
+        res.render('dogcreate', { title: 'dog Create' });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+}
+
+// Handle building the view for updating a dog.
+// query provides the id
+exports.dog_update_Page = async function (req, res) {
+    console.log("update view for item " + req.query.id)
+    try {
+        let result = await dog.findById(req.query.id)
+        res.render('dogupdate', { title: 'dog Update', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle a delete one view with id from query
+exports.dog_delete_Page = async function (req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try {
+        result = await dog.findById(req.query.id)
+        res.render('dogdelete', {title: 'dog Delete', toShow:result});
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
--- routes/dog.js
+++ routes/dog.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const dog_controllers = require('../controllers/dog');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,4 +9,15 @@ router.get('/', function(req, res, next) {
   router.get('/dog/:id', dog_controller.dog_detail)
 });
 
+/* GET detail dog page */
+router.get('/detail', dog_controllers.dog_view_one_Page);
 module.exports = router;
+
+/* GET create dog page */
+router.get('/create', dog_controllers.dog_create_Page);
+
+/* GET create update page */
+router.get('/update', dog_controllers.dog_update_Page);
+
+/* GET delete dog page */
+router.get('/delete', dog_controllers.dog_delete_Page);
