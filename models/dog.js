8f268b50914f3a5bd79a63772db92f8b57f6a43b
nikhithasree <78521584+nikhithasree@users.noreply.github.com>
2023-11-29 21:24:21 -0600

    Updated dogs.js

--- models/dog.js
+++ models/dog.js
@@ -1,8 +1,36 @@
-//const { Double } = require("mongodb")
 const mongoose = require("mongoose")
 const dogSchema = mongoose.Schema({
-breed: String,
-color: String,
-cost:Number,
+
+breed: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 20,
+    
+    validate: {
+        validator: function (v) {
+            return /^[a-zA-Z0-9_]+$/.test(v); 
+        },
+        message: props => `${props.value} is not a valid username! Use only letters, numbers, and underscores.`,
+    },
+},
+color: {
+    type: String,
+    validate: {
+        validator: function(v) {
+            return /^[a-zA-Z0-9_]+$/.test(v); 
+        },
+        message: props => `${props.value} is not a valid price! Price must be a non-negative number.`,
+    },
+},
+cost: {
+    type: Number,
+    validate: {
+        validator: function(v) {
+            return v >= 0; 
+        },
+        message: props => `${props.value} is not a valid price! Price must be a non-negative number.`,
+    },
+},
 })
 module.exports = mongoose.model("dog",dogSchema)
